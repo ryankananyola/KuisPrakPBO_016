9db71eb2ee01ffc67f94d208e72f9f22b2725cc5
ryankananyola <mariadestan2027@gmail.com>
2025-03-17 18:47:23 +0700

    update transaksi

--- src/View/HalamanPembelian.java
+++ src/View/HalamanPembelian.java
@@ -11,8 +11,6 @@ public class HalamanPembelian extends JFrame {
         setLayout(null);
         setLocationRelativeTo(null);
         
-        getContentPane().setBackground(Color.PINK);
-        
         int hargaApel = 15000 * jumlahApel;
         int hargaJeruk = 12000 * jumlahJeruk;
         int hargaMangga = 20000 * jumlahMangga;
@@ -21,31 +19,31 @@ public class HalamanPembelian extends JFrame {
         int total = subtotal + pajak;
         
         JLabel labelApel = new JLabel("Apel: " + jumlahApel + " kg x Rp15.000 = Rp" + hargaApel);
-        labelApel.setBounds(20, 20, 300, 20);
+        labelApel.setBounds(35, 20, 300, 20);
         add(labelApel);
         
         JLabel labelJeruk = new JLabel("Jeruk: " + jumlahJeruk + " kg x Rp12.000 = Rp" + hargaJeruk);
-        labelJeruk.setBounds(20, 50, 300, 20);
+        labelJeruk.setBounds(35, 50, 300, 20);
         add(labelJeruk);
         
         JLabel labelMangga = new JLabel("Mangga: " + jumlahMangga + " kg x Rp20.000 = Rp" + hargaMangga);
-        labelMangga.setBounds(20, 80, 300, 20);
+        labelMangga.setBounds(35, 80, 300, 20);
         add(labelMangga);
         
         JLabel labelSubtotal = new JLabel("Subtotal: Rp" + subtotal);
-        labelSubtotal.setBounds(20, 130, 300, 20);
+        labelSubtotal.setBounds(35, 130, 300, 20);
         add(labelSubtotal);
         
         JLabel labelPajak = new JLabel("Pajak (10%): Rp" + pajak);
-        labelPajak.setBounds(20, 160, 300, 20);
+        labelPajak.setBounds(35, 160, 300, 20);
         add(labelPajak);
         
         JLabel labelTotal = new JLabel("Total Harga: Rp" + total);
-        labelTotal.setBounds(20, 190, 300, 20);
+        labelTotal.setBounds(35, 190, 300, 20);
         add(labelTotal);
         
         JButton btnKembali = new JButton("Kembali");
-        btnKembali.setBounds(100, 240, 200, 30);
+        btnKembali.setBounds(35, 230, 310, 30);
         btnKembali.addActionListener(e -> {
             new HalamanUtama();
             dispose();
